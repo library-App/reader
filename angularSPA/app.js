696700eb6948b7476c116a4ca21f3c2a7e9e2992
web-kk <308552493@qq.com>
2016-11-21 17:43:24 +0800

    v

--- angularSPA/app.js
+++ angularSPA/app.js
@@ -1,39 +1,39 @@
-var app=angular.module('reader',['ngRoute']);
+var app = angular.module('reader', ['ngRoute']);
 app.config([
 	'$routeProvider',
-	function($routeProvider){
-		$routeProvider.when('/',{
-			templateUrl:'reader/login/login.html',
-			controller:'loginCtrl'
-<<<<<<< HEAD
-		}).when('/bookshelf',{
-			templateUrl:'reader/bookshelf/bookshelf.html',
-			controller:''
-		}).when('/readPage',{
-			templateUrl:'reader/readPages/readPage.html',
-			controller:'readCtrl'
-		}).when('/bookmark',{
-			templateUrl:'reader/readPages/bookmark.html',
-			controller:'markCtrl'
-		}).when('/notes',{
-			templateUrl:'reader/readPages/note.html',
-			controller:'notCtrl'
-=======
-		}).when('/select',{
-			templateUrl:'reader/bookcity/select.html',
-			controller:'selectCtrl'
-		}).when('/super',{
-			templateUrl:'reader/bookcity/super.html',
-			controller:'superCtrl'
-		}).when('/newbook',{
-			templateUrl:'reader/bookcity/newbook.html'
-		}).when('/list',{
-			templateUrl:'reader/bookcity/list.html'
-		}).when('/classify',{
-			templateUrl:'reader/bookcity/classify.html'
-		}).when('/bookshow',{
-			templateUrl:'reader/bookcity/bookshow.html'
->>>>>>> 1a73ad8dd19f02fc6da6747ad79ee31f99fdd648
+	function($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'reader/login/login.html',
+			controller: 'loginCtrl'
+
+		}).when('/bookshelf', {
+			templateUrl: 'reader/bookshelf/bookshelf.html',
+			controller: ''
+		}).when('/readPage', {
+			templateUrl: 'reader/readPages/readPage.html',
+			controller: 'readCtrl'
+		}).when('/bookmark', {
+			templateUrl: 'reader/readPages/bookmark.html',
+			controller: 'markCtrl'
+		}).when('/notes', {
+			templateUrl: 'reader/readPages/note.html',
+			controller: 'notCtrl'
+
+		}).when('/select', {
+			templateUrl: 'reader/bookcity/select.html',
+			controller: 'selectCtrl'
+		}).when('/super', {
+			templateUrl: 'reader/bookcity/super.html',
+			controller: 'superCtrl'
+		}).when('/newbook', {
+			templateUrl: 'reader/bookcity/newbook.html'
+		}).when('/list', {
+			templateUrl: 'reader/bookcity/list.html'
+		}).when('/classify', {
+			templateUrl: 'reader/bookcity/classify.html'
+		}).when('/bookshow', {
+			templateUrl: 'reader/bookcity/bookshow.html'
+
 		})
 	}
 ])
